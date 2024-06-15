9b26d6ba8b0943c29ea6182b204b5787d21e2ec7
Varinia Butcher <varinia_b@hotmail.com>
2024-06-15 13:19:13 +0000

    Add user request loop to repeat request until data provided is valid

--- run.py
+++ run.py
@@ -29,15 +29,23 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    #while loop that asks user for data 
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")  # asks for data input from user here
 
-def validate_data(values):
+        sales_data = data_str.split(",") #converts data from user into a list of values
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    return sales_data
+
+
+def validate_data(values): #function that checks for errors in inputted values, if none, it returns true and returns string inside above if statement
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
@@ -50,8 +58,11 @@ def validate_data(values):
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
                 )
-    except ValueError as e:
+    except ValueError as e: #if validate_data function encounters an error, the below code is run, returns false and the while loop will run again 
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
+
+data = get_sales_data()
 
